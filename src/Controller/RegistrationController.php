890d371c0315fa6d667b75e2a411556c3881232f
jonathan <jonathancastro2018@yahoo.com>
2021-08-06 17:39:18 -0400

    cambios registrar

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -72,7 +72,7 @@ class RegistrationController extends AbstractController
 
             // return $this->redirectToRoute('dashboard');
 
-            return $this->redirectToRoute('user_dashboard ');
+            return $this->redirectToRoute('user_dashboard');
         }
 
         return $this->render('registration/register.html.twig', [
