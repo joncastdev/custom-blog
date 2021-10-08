c0e876c9571725f282c985b98ec3c0d24ec864e5
jonathan <jonathancastro2018@yahoo.com>
2021-10-08 09:20:01 -0400

    avance con seo y secciones

---
+++ src/Controller/CodeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CodeController extends AbstractController
+{
+    /**
+     * @Route("/codigos-de-programacion", name="codigos")
+     */
+    public function index(): Response
+    {        
+
+        return $this->render('code/index.html.twig', [
+            'controller_name' => 'CodeController',
+        ]);
+    }
+
+   
+}
